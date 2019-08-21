4cc5ea4e6259c9f22c6fea88c392d05be9304eee
Eulalio <eulalio_cruz_nava@hotmail.com>
2019-08-21 15:17:48 -0500

    cambios en el login y creacion del servicio

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BoundAttribute } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  autorizar:boolean;
 
-  constructor() { }
+  public  form = {
+    usuario:null,
+    password:null
+  };
+  public auto ={
+    autorizacion:null
+  }
+
+  constructor() {
+    //variable para activar los formularios
+    this.autorizar=false;
+  }
 
   ngOnInit() {
+  }
+  onSubmit(){
+    console.log(this.form);
+  }
+
+  //Evento que valida el logeo por auntorizacion mv
+  ingresar(){
+    if(this.auto.autorizacion != null){
+      alert(this.auto.autorizacion);
+    }else{
+      alert("Debes digitar en numero de autorizacion");
+    }
+  }
+
+  verAutorizacion(){
+    this.autorizar = true;
+  }
+
+  verProvedor(){
+    this.autorizar = false;
   }
 
 }
